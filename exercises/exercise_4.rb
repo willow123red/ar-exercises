27421cbee58c73bdbd97e19b6b0ac5de7a8a8d65
Lucas <ljwillow@hotmail.com>
2019-09-19 00:29:41 +0000

    add stores, query stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,13 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts store.name, store.annual_revenue
+end
+
+Store.where("womens_apparel = :womens_apparel and annual_revenue < :annual_revenue", { womens_apparel: true, annual_revenue: 1000000 })
